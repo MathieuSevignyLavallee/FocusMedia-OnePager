c084a809b4ab956edff8820481d1cf9c1bc81d74
MathieuSevignyLavallee <89943988+MathieuSevignyLavallee@users.noreply.github.com>
2024-05-30 11:20:29 -0400

    vercel analytics

--- focusmedia/src/App.js
+++ focusmedia/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './layout/Header';
 import Main from './layout/Main';
 import Footer from './layout/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Analytics/>
       <Footer />
     </div>
   );
